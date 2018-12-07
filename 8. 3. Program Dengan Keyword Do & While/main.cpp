e6ab33890f8b9a4ac52bab041ead035aa875aedc
M-Shiddiqy-Nurramadhani-A-R-Y <45529861+M-Shiddiqy-Nurramadhani-A-R-Y@users.noreply.github.com>
2018-12-08 05:52:26 +0700

    Program C++ ( Untuk Pemula )

---
+++ 8. 3. Program Dengan Keyword Do & While/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+int nilai;
+
+cout <<"-----------------------------------"<<endl;
+cout <<"_Program Dengan Keyword Do & While_"<<endl;
+cout <<"-----------------------------------"<<endl<<endl;
+
+cout <<"Masukan salah satu nilai dari 0-10"<<endl;
+cout <<"Masukkan nilainya : ";  cin>>nilai;
+cout <<endl;
+
+do
+{
+    cout << nilai <<endl;
+    nilai++;
+}
+while(nilai<=10);
+
+cout <<"Selesai"<<endl;
+
+getch();
+return 0;
+}
